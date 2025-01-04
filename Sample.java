f8bbb4a1386b08bb2aaecfea94eaf238810dfcf0
Bhuvana kruthi <bhuvanakruthigodugug@gmail.com>
2025-01-04 13:31:47 +0530

    This is the simple java program

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample
+ {
+      public static void main(String[] args) {
+           int n= 51;
+     //checking whether the number is even or odd
+     if (n% 2 == 0)
+              System.out.println(n + " is Even");
+     else
+              System.out.println(n + " is odd");
+      }
+ }
